a9fa13e0e40aa2ba5b1f10bd21401bf48876e289
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-02-15 19:09:18 +0100

    Add missing content-length

--- http-routing/main.go
+++ http-routing/main.go
@@ -17,6 +17,8 @@ var validMethods = map[string]string{
 const (
 	badRequestError = iota
 	internalServerError
+	pageNotFound
+	resourceNotFound
 )
 
 func main() {
@@ -70,16 +72,18 @@ func handleReq(conn net.Conn) {
 	err := validateMethod(method)
 
 	if err != nil {
-		handleError(conn, err, badRequestError)
+		sendError(conn, err, badRequestError)
 	} else {
-		handleSuccess(conn, method, path)
+		handleRequest(conn, method, path)
 	}
 
 }
 
 func validateMethod(method string) error {
+	fmt.Printf("Testing Method: %s\n", method)
+
 	valid := false
-	for v := range validMethods {
+	for _, v := range validMethods {
 		if v == method {
 			valid = true
 		}
@@ -92,32 +96,49 @@ func validateMethod(method string) error {
 	return nil
 }
 
-func handleError(conn net.Conn, err error, errType int) {
+func sendError(conn net.Conn, err error, errType int) {
 	var status int
 	var reason string
+
+	response := err.Error()
 
 	switch errType {
 	case badRequestError:
 		status = 400
 		reason = "BadRequest"
+	case resourceNotFound:
+		status = 404
+		reason = "ResourceNotFound"
+	case pageNotFound:
+		status = 404
+		reason = "PageNotFound"
+		response = `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body><strong>Page Not Found</strong></body></html>`
 	case internalServerError:
+	default:
 		status = 500
 		reason = "InternalServerError"
 	}
 
+	fmt.Printf("Sending error response: %v %v %v\n", status, reason, err.Error())
+
 	fmt.Fprintf(conn, "HTTP/1.1 %d %s\r\n", status, reason)
 	fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(response))
 	fmt.Fprintf(conn, "\r\n")
-	fmt.Fprintf(conn, "")
+	fmt.Fprintf(conn, response)
 }
 
-func handleSuccess(conn net.Conn, method, path string) {
+func handleRequest(conn net.Conn, method, path string) {
 	if method == validMethods["get"] {
 		switch path {
 		case "/":
 			sendSuccess(conn, `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body><strong>Home</strong></body></html>`)
 		case "/about":
 			sendSuccess(conn, `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body><strong>About</strong></body></html>`)
+		case "/contact":
+			sendSuccess(conn, `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title></title></head><body><strong>Contact Us</strong></body></html>`)
+		default:
+			sendError(conn, errors.New("Page Not Found"), pageNotFound)
 		}
 	} else {
 		switch path {
@@ -125,13 +146,17 @@ func handleSuccess(conn net.Conn, method, path string) {
 			sendSuccess(conn, "User created")
 		case "/post":
 			sendSuccess(conn, "Post created")
+		default:
+			sendError(conn, fmt.Errorf("Route %s is not registered", path), resourceNotFound)
 		}
 	}
 }
 
 func sendSuccess(conn net.Conn, body string) {
+	fmt.Printf("Sending success response: %v\n", body)
 	fmt.Fprintf(conn, "HTTP/1.1 200 Ok\r\n")
 	fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprintf(conn, "\r\n")
 	fmt.Fprintf(conn, body)
 }
